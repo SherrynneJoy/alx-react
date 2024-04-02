2b89666fd455c8df7f78a3e79f3b2359805497ad
SherrynneJoy <sherrynnembuvi@gmail.com>
2024-04-02 09:40:42 +0000

    Basic setup

---
+++ 0x00-Webpack/task_0/src/index.js
@@ -0,0 +1,9 @@
+import $ from "jquery";
+
+function addElementsToBody() {
+	$('body').append('<p>Holberton Dashboard</p>');
+	$('body').append('<p>Dashboard data for the students</p>');
+	$('body').append('<p>Copyright - Holberton School</p>');
+}
+
+addElementsToBody();

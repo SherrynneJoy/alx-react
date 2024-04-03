7d504a9c351fa3984562e2c021a64dc62146c0fc
SherrynneJoy <sherrynnembuvi@gmail.com>
2024-04-03 10:40:48 +0000

    Embedding expressions, functions

---
+++ 0x01-react_intro/task_1/dashboard/src/utils.js
@@ -0,0 +1,17 @@
+function getFullYear = () => {
+  let current_year = new Date().getFullYear();
+  return current_year;
+}
+
+function getFooterCopy = (isIndex) => {
+  if (isIndex) {
+    return "Holberton School";
+  } else {
+    return "Holberton School main dashboard";
+  }
+}
+
+module.exports = {
+  getFullYear,
+  getFooterCopy,
+};

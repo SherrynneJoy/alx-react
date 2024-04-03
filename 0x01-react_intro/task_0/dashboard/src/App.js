c75c26da21b43183210c9eb700e9554a507f35a2
SherrynneJoy <sherrynnembuvi@gmail.com>
2024-04-03 09:04:05 +0000

    Basic application

--- 0x01-react_intro/task_0/dashboard/src/App.js
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -4,21 +4,19 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+	<h1>School Dashboard</h1>
+      </div>
+      <div className="App-body">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Login to access the full dashboard
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </div>
+      <div className="App-footer">
+        <p>Copyright 2020 - holberton School</p>
+      </div>
+   </div>
   );
 }
 

b1d1c6517d542269ef90a1a7442794baef3ffc6a
SherrynneJoy <sherrynnembuvi@gmail.com>
2024-04-04 13:55:37 +0000

    Deploy to a GitHub page

---
+++ 0x01-react_intro/task_4/dashboard/src/utils.js
@@ -0,0 +1,21 @@
+function getFullYear = () => {
+  let current_year = new Date().getFullYear();
+  return current_year;
+}
+
+function getFooterCopy = (isIndex) => {
+  if (isIndex) {
+    return "Holberton School";
+  } else {
+    return "Holberton School main dashboard";
+  }
+}
+
+function getLatestNotification = () => {
+  return "<strong>Urgent requirement</strong> - complete by EOD";
+}
+module.exports = {
+  getFullYear,
+  getFooterCopy,
+  getLatestNotification,
+};

9df0f317bee328a302247c1df4f27b1321b28642
SherrynneJoy <sherrynnembuvi@gmail.com>
2024-04-06 11:25:34 +0000

    Basic components

---
+++ 0x02-react_props/task_0/dashboard/src/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import holberton_logo from "../assets/holberton_logo.jpg";
+import './Header.css';
+
+export default function Header() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>School Dashboard</h1>
+      </div>
+  );
+}

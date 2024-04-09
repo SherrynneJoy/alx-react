936bcfe20aaafed0032cd2df0678688c5c88ea45
SherrynneJoy <sherrynnembuvi@gmail.com>
2024-04-09 19:41:20 +0300

    Commence with class components

--- 0x03-React_component/task_0/dashboard/src/App/App.js
+++ 0x03-React_component/task_0/dashboard/src/App/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Notifications from '../Notifications/Notifications';
@@ -7,24 +8,28 @@ import Footer from '../Footer/Footer';
 import CourseList from "../CourseList/CourseList";
 import PropTypes from "prop-types";
 
-function App() {
-  return (
-    <>
-      <Notifications />
-      <div className="App">
-        <Header />
-      </div>
-      <div className="App-body">{!isLoggedIn ? <Login /> : <CourseList />}
-      </div>
-      <div className="App-footer">
-	<Footer />
-      </div>
-    </>
-  );
+class App extends React.Component {
+	render() {
+		const { isLoggedIn } = this.props; // Access props using this.props, not this.propTypes
+		return (
+			<>
+				<Notifications />
+				<div className="App">
+					<Header />
+				</div>
+				<div className="App-body">
+					{!isLoggedIn ? <Login /> : <CourseList />}
+				</div>
+				<div className="App-footer">
+					<Footer />
+				</div>
+			</>
+		);
+	}
 }
 
 const listCourses = [
-{
+	{
 		id: 1,
 		name: 'ES6',
 		credit: '60'
@@ -39,7 +44,7 @@ const listCourses = [
 		name: 'React',
 		credit: '40'
 	}
-]
+];
 
 const listNotifications = [
 	{
@@ -55,11 +60,11 @@ const listNotifications = [
 	{
 		id: 3,
 		html: {
-			__html: getLatestNotification()
+			__html: 'Some notification content' // You need to define or import getLatestNotification function
 		},
 		type: "urgent",
 	}
-]
+];
 
 App.defaultProps = {
 	isLoggedIn: false,

29151814f8d9420d41c47c6a5a74a53515757fb5
SherrynneJoy <125467595+SherrynneJoy@users.noreply.github.com>
2024-04-09 17:03:48 +0300

    task_5

---
+++ 0x02-react_props/task_5/dashboard/src/App/App.js
@@ -0,0 +1,72 @@
+import logo from './logo.svg';
+import './App.css';
+import Notifications from '../Notifications/Notifications';
+import Header from '../Header/Header';
+import Login from '../Login/Login';
+import Footer from '../Footer/Footer';
+import CourseList from "../CourseList/CourseList";
+import PropTypes from "prop-types";
+
+function App() {
+  return (
+    <>
+      <Notifications />
+      <div className="App">
+        <Header />
+      </div>
+      <div className="App-body">{!isLoggedIn ? <Login /> : <CourseList />}
+      </div>
+      <div className="App-footer">
+	<Footer />
+      </div>
+    </>
+  );
+}
+
+const listCourses = [
+{
+		id: 1,
+		name: 'ES6',
+		credit: '60'
+	},
+	{
+		id: 2,
+		name: 'Webpack',
+		credit: '20'
+	},
+	{
+		id: 3,
+		name: 'React',
+		credit: '40'
+	}
+]
+
+const listNotifications = [
+	{
+		id: 1,
+		type: "default",
+		value: "New course available"
+	},
+	{
+		id: 2,
+		type: "urgent",
+		value: "New resume available"
+	},
+	{
+		id: 3,
+		html: {
+			__html: getLatestNotification()
+		},
+		type: "urgent",
+	}
+]
+
+App.defaultProps = {
+	isLoggedIn: false,
+};
+
+App.propTypes = {
+	isLoggedIn: PropTypes.bool,
+};
+
+export default App;

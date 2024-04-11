ea4e34bef1ad4cdf08f2008b59d7ff9ebc92f79d
SherrynneJoy <sherrynnembuvi@gmail.com>
2024-04-11 11:41:36 +0000

    Lifecycles

--- 0x03-React_component/task_1/dashboard/src/App/App.test.js
+++ 0x03-React_component/task_1/dashboard/src/App/App.test.js
@@ -35,11 +35,9 @@ describe('<App />', () => {
 	  expect(wrapper.find("CourseList")).toHaveLength(1);
 	});
 	it("calls logOut function when Ctrl + h keys are pressed", () => {
-	  const logOut = jest.fn(() => undefined);
-	  const wrapper = shallow(<App logOut={logOut} />);
-	  const alert = jest.spyOn(globalo, 'alert');
-	  expect(alert);
-	  expect(logOut);
-	  jest.restoreAllMocks();
-  });
+	  const event = new KeyboardEvent('keydown', { key: 'h', ctrlKey: true });
+	  document.dispatchEvent(event);
+	  expect(mockLogOut).toHaveBeenCalled();
+	  expect(window.alert).toHaveBeenCalledWith('Logging you out');
+	});
 });

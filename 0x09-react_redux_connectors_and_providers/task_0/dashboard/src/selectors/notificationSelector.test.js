5c5073c80830c3b489d8027fc11a2fdb980df742
SherrynneJoy <sherrynnembuvi@gmail.com>
2024-05-11 09:32:53 +0000

    Write mapStateToProps

---
+++ 0x09-react_redux_connectors_and_providers/task_0/dashboard/src/selectors/notificationSelector.test.js
@@ -0,0 +1,47 @@
+import { filterTypeSelected, getNotifications, getUnreadNotifications } from './notificationSelector';
+import { FETCH_NOTIFICATIONS_SUCCESS, MARK_AS_READ } from '../actions/notificationActionTypes';
+import { notificationsNormalizer } from '../schema/notifications';
+import { notificationReducer } from '../reducers/notificationReducer';
+import { expect as expectChai } from 'chai';
+
+var _ = require('lodash');
+const { Map, fromJS } = require('immutable');
+
+describe("tests for notificationSelector", () => {
+	const data = [
+		{ id: 1, type: "default", value: "New course available" },
+		{ id: 2, type: "urgent", value: "New resume available" },
+		{ id: 3, type: "urgent", value: "New data available" }
+	];
+	const state = fromJS({
+		filter: "DEFAULT",
+		notifications: notificationsNormalizer([
+			{ id: 1, isRead: false, type: "default", value: "New course available" },
+			{ id: 2, isRead: false, type: "urgent", value: "New resume available" },
+			{ id: 3, isRead: false, type: "urgent", value: "New data available" }
+		]).notifications
+	});
+	it("tests that filterTypeSelected works well", (done) => {
+		const result = filterTypeSelected(notificationReducer(undefined, {}));
+		expectChai(_.isEqual(result, 'DEFAULT')).to.equal(true);
+		done();
+	});
+	it("tests that getNotifications returns a list of the message entities within the reducer", (done) => {
+		const result = getNotifications(notificationReducer(undefined, { type: FETCH_NOTIFICATIONS_SUCCESS, data: data }));
+		const expected = notificationsNormalizer([
+			{ id: 1, isRead: false, type: "default", value: "New course available" },
+			{ id: 2, isRead: false, type: "urgent", value: "New resume available" },
+			{ id: 3, isRead: false, type: "urgent", value: "New data available" }
+		]);
+		expectChai(_.isEqual(result, expected.notifications)).to.equal(true);
+		done();
+	});
+	it("tests that that getUnreadNotifications return a list of the message entities within the reducer", (done) => {
+		const result = getUnreadNotifications(notificationReducer(state, { type: MARK_AS_READ, index: 2 }));
+		const expected = [
+			{ id: 2, isRead: true, type: 'urgent', value: 'New resume available' }
+		];
+		expectChai(_.isEqual(result, expected)).to.equal(true);
+		done();
+	});
+});
